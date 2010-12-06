ad89ad4c46d5e459881461929c32b2d5dc3a4ccb
Jaap van der Meer <jaapvandermeer@gmail.com>
2010-12-06 10:46:47 +0100

    Published version 1.0.2

--- qislider.js
+++ qislider.js
@@ -1,3 +1,71 @@
+
+/**
+ * EventDispatcher is a class that implements the subscriber pattern. This
+ * class has a method +dispatchEvent(type)+ that can dispatch an event.
+ *
+ * Other classes can subscribe to these events by doing .addEventListener().
+ *
+ * Note: to preserve the +this+ or current scope. Use jQuery.proxy.
+ *
+ * @see jQuery.proxy
+ * @responsible Jaap van der Meer
+ */
+var EventDispatcher = Class.extend({
+
+  /**
+   * Subscribes to an event. If this event is fired, func is called. See note
+   * about jQuery.proxy to proxy the current scope.
+   *
+   * @param [event] The event type.
+   * @param [func] The function that will be fired.
+   */
+	addEventListener:function(event, func){
+    if(!this.eventListeners)
+      this.eventListeners = {};
+
+    if(!this.eventListeners[event]){
+      this.eventListeners[event] = [];
+    }
+    this.eventListeners[event].push(func);
+    return this;
+  },
+
+  /**
+   * Removes listening to a specific function.
+   *
+   * @param [event] The event type.
+   * @param [func] The function that was listening.
+   */
+  removeEventListener: function(event, func){
+    if(!this.eventListeners[event])
+      return;
+    for(var i = 0, len = this.eventListeners[event].length; i < len; i+=1){
+      if (this.eventListeners[event][i] == func) {
+        this.eventListeners[event].splice(i, 1);
+      }
+    }
+    return this;
+  },
+
+  /**
+   * Dispatches an event. Event is just a string.
+   * @param [event] The event type.
+   */
+  dispatchEvent:function(event) {
+    var args = [];
+    for(var i = 1, len = arguments.length; i < len; i+=1){
+      args.push(arguments[i]);
+    }
+
+    if(this.eventListeners && this.eventListeners[event]) {
+      for(var j = 0, len = this.eventListeners[event].length; j < len; j+=1){
+        this.eventListeners[event][j].apply(this, args);
+      }
+    }
+    return this;
+  }
+});
+
 /**
  * The slider class is responsible for drawing the slider and maintaining
  * all states of the slider.
@@ -777,10 +845,16 @@ var AdvancedSliderView = EventDispatcher.extend({
     this.topRowElement = $('<div></div>').addClass('toprow');
     this.element.append(this.topRowElement);
 
-    this.name = this.options.name || "untitled";
-    this.nameElement = $('<div class="name"></div>').html(this.name);
-    this.nameElement.bind('selectstart', function() {return false;});
 
+    if(this.options.element)
+      this.nameElement = $('.name', this.options.element);
+
+    if(!this.nameElement || this.nameElement.length == 0) {
+      this.name = this.options.name || "untitled";
+      this.nameElement = $('<div class="name"></div>').html(this.name);
+    }
+
+    this.nameElement.bind('selectstart', function() {return false;});
     this.sliderElementsElement = $('<div></div>').addClass('sliderElements');
     this.topRowElement.append(this.sliderElementsElement);
     this.topRowElement.append(this.nameElement);
